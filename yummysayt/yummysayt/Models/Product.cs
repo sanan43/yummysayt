040d4bcdccb5562816d80077750434d0bee5b066
tural003 <turaleliyev702@gmail.com>
2023-05-20 11:17:25 +0400

    first commit

---
+++ yummysayt/yummysayt/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace yummysayt.Models
+{
+    public class Product
+    {
+        
+        public int Id { get; set; }
+        public string ProductImage { get; set; }
+        [Required]
+        [MaxLength(60)]
+        public string Name { get; set; }
+        public string Ingredient { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+    }
+}
